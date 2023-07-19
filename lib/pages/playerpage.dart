9f300fbf9b29fbac21ae5214238cb6bc1221092d
Tosin2289 <ayoolatosin00@gmail.com>
2023-07-19 23:07:37 +0100

    redesign

--- lib/pages/playerpage.dart
+++ lib/pages/playerpage.dart
@@ -29,177 +29,177 @@ class _PlayerPageState extends State<PlayerPage> {
             children: [
               Obx(
                 () => Expanded(
-                  child: Container(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    height: 300,
-                    width: 300,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    alignment: Alignment.center,
-                    child: QueryArtworkWidget(
-                      id: widget.data[controller.playIndex.value].id,
-                      type: ArtworkType.AUDIO,
-                      artworkHeight: double.infinity,
-                      artworkWidth: double.infinity,
-                      nullArtworkWidget: const Icon(
-                        Icons.music_note,
-                        size: 54,
-                        color: whiteColor,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                      color: whiteColor,
-                      borderRadius:
-                          BorderRadius.vertical(top: Radius.circular(16))),
                   child: Column(
                     children: [
-                      Text(
-                        widget
-                            .data[controller.playIndex.value].displayNameWOExt,
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                        style: const TextStyle(
-                            color: bgDarkColor,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Text(
-                        "${widget.data[controller.playIndex.value].artist}",
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                        style: const TextStyle(
-                            color: bgDarkColor,
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Obx(
-                        () => Row(
-                          children: [
-                            Text(
-                              controller.position.value,
-                              style: const TextStyle(
-                                color: bgDarkColor,
-                              ),
-                            ),
-                            Expanded(
-                              child: Slider(
-                                  thumbColor: sliderColor,
-                                  activeColor: sliderColor,
-                                  inactiveColor: bgColor,
-                                  min: const Duration(seconds: 0)
-                                      .inSeconds
-                                      .toDouble(),
-                                  max: controller.max.value,
-                                  value: controller.value.value,
-                                  onChanged: (newValue) {
-                                    controller.changeDurationToSeconds(
-                                        newValue.toInt());
-                                    newValue = newValue;
-                                  }),
-                            ),
-                            Text(
-                              controller.duration.value,
-                              style: const TextStyle(color: bgDarkColor),
-                            )
-                          ],
+                      Container(
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        height: 300,
+                        width: 300,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                        alignment: Alignment.center,
+                        child: QueryArtworkWidget(
+                          id: widget.data[controller.playIndex.value].id,
+                          type: ArtworkType.AUDIO,
+                          artworkHeight: double.infinity,
+                          artworkWidth: double.infinity,
+                          nullArtworkWidget: const Icon(
+                            Icons.music_note,
+                            size: 54,
+                            color: whiteColor,
+                          ),
                         ),
                       ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      Column(
                         children: [
-                          IconButton(
-                            onPressed: () {
-                              controller.playSong(
-                                  widget
-                                      .data[controller.playIndex.value - 1].uri,
-                                  controller.playIndex.value - 1);
-                            },
-                            icon: const Icon(
-                              Icons.skip_previous_rounded,
-                              size: 40,
-                              color: bgDarkColor,
-                            ),
+                          Text(
+                            widget.data[controller.playIndex.value]
+                                .displayNameWOExt,
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            style: const TextStyle(
+                                color: bgDarkColor,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            "${widget.data[controller.playIndex.value].artist}",
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            style: const TextStyle(
+                                color: bgDarkColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal),
+                          ),
+                          const SizedBox(
+                            height: 12,
                           ),
                           Obx(
-                            () => CircleAvatar(
-                              radius: 35,
-                              backgroundColor: bgDarkColor,
-                              child: Transform.scale(
-                                  scale: 2.5,
-                                  child: controller.isPlaying.value
-                                      ? IconButton(
-                                          onPressed: () {
-                                            if (controller.isPlaying.value) {
-                                              controller.audioPlayer.pause();
-                                              controller.isPlaying(false);
-                                            } else {
-                                              controller.audioPlayer.play();
-                                              controller.isPlaying(true);
-                                            }
-                                          },
-                                          icon: const Icon(
-                                            Icons.pause,
-                                            color: whiteColor,
-                                          ),
-                                        )
-                                      : IconButton(
-                                          onPressed: () {
-                                            if (controller.isPlaying.value) {
-                                              controller.audioPlayer.pause();
-                                              controller.isPlaying(false);
-                                            } else {
-                                              controller.audioPlayer.play();
-                                              controller.isPlaying(true);
-                                            }
-                                          },
-                                          icon: const Icon(
-                                            Icons.play_arrow,
-                                            color: whiteColor,
-                                          ),
-                                        )),
+                            () => Row(
+                              children: [
+                                Text(
+                                  controller.position.value,
+                                  style: const TextStyle(
+                                    color: bgDarkColor,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Slider(
+                                      thumbColor: sliderColor,
+                                      activeColor: sliderColor,
+                                      inactiveColor: bgColor,
+                                      min: const Duration(seconds: 0)
+                                          .inSeconds
+                                          .toDouble(),
+                                      max: controller.max.value,
+                                      value: controller.value.value,
+                                      onChanged: (newValue) {
+                                        controller.changeDurationToSeconds(
+                                            newValue.toInt());
+                                        newValue = newValue;
+                                      }),
+                                ),
+                                Text(
+                                  controller.duration.value,
+                                  style: const TextStyle(color: bgDarkColor),
+                                )
+                              ],
                             ),
                           ),
-                          IconButton(
-                            onPressed: () {
-                              controller.playSong(
-                                  widget
-                                      .data[controller.playIndex.value + 1].uri,
-                                  controller.playIndex.value + 1);
-                            },
-                            icon: const Icon(
-                              Icons.skip_next_rounded,
-                              size: 40,
-                              color: bgDarkColor,
-                            ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  controller.playSong(
+                                      widget
+                                          .data[controller.playIndex.value - 1]
+                                          .uri,
+                                      controller.playIndex.value - 1);
+                                },
+                                icon: const Icon(
+                                  Icons.skip_previous_rounded,
+                                  size: 40,
+                                  color: bgDarkColor,
+                                ),
+                              ),
+                              Obx(
+                                () => CircleAvatar(
+                                  radius: 35,
+                                  backgroundColor: bgDarkColor,
+                                  child: Transform.scale(
+                                      scale: 2.5,
+                                      child: controller.isPlaying.value
+                                          ? IconButton(
+                                              onPressed: () {
+                                                if (controller
+                                                    .isPlaying.value) {
+                                                  controller.audioPlayer
+                                                      .pause();
+                                                  controller.isPlaying(false);
+                                                } else {
+                                                  controller.audioPlayer.play();
+                                                  controller.isPlaying(true);
+                                                }
+                                              },
+                                              icon: const Icon(
+                                                Icons.pause,
+                                                color: whiteColor,
+                                              ),
+                                            )
+                                          : IconButton(
+                                              onPressed: () {
+                                                if (controller
+                                                    .isPlaying.value) {
+                                                  controller.audioPlayer
+                                                      .pause();
+                                                  controller.isPlaying(false);
+                                                } else {
+                                                  controller.audioPlayer.play();
+                                                  controller.isPlaying(true);
+                                                }
+                                              },
+                                              icon: const Icon(
+                                                Icons.play_arrow,
+                                                color: whiteColor,
+                                              ),
+                                            )),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  controller.playSong(
+                                      widget
+                                          .data[controller.playIndex.value + 1]
+                                          .uri,
+                                      controller.playIndex.value + 1);
+                                },
+                                icon: const Icon(
+                                  Icons.skip_next_rounded,
+                                  size: 40,
+                                  color: bgDarkColor,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
                 ),
+              ),
+              const SizedBox(
+                height: 12,
               ),
             ],
           ),
